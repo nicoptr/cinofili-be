f54425d2975f824f12dd8bfb771e67f0fba7fd22
nicola_ptr <nicola.pietrangelo@overzoom.it>
2025-11-06 15:22:38 +0100

    Add missing semicolons

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -2,13 +2,13 @@ import { DateTime } from "luxon";
 import {undefined} from "zod";
 
 export const formatItaliaDate = (date: Date | undefined | null): string => {
-    return date ? DateTime.fromJSDate(date).toFormat("dd-MM-yyyy") : "data mancante"
+    return date ? DateTime.fromJSDate(date).toFormat("dd-MM-yyyy") : "data mancante";
 }
 
 export const formatItaliaTime = (date: Date | undefined | null): string => {
     return new Date(date ?? "").toLocaleTimeString("it-IT", {
         hour: "2-digit",
         minute: "2-digit"
-    })
+    });
 
 }
